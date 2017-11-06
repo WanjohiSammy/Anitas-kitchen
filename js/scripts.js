4b2d9be3cc0400c64ce688890c9462acb2296836
murungiKirima <murungikirima@gmail.com>
2017-11-06 18:04:24 +0300

    Add analytics

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,11 @@ function openNav() {
 function closeNav() {
     document.getElementById("Nav").style.width = "0%";
 }
+
+
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)};
+  gtag('js', new Date());
+
+ gtag('config', 'GA_TRACKING_ID');
